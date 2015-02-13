5163d5b1f75d700d2a53bf7bad7389071537420d
eq <armand_bertea@yahoo.co.uk>
2015-02-13 12:22:28 +0200

    Am facut corectat fisier return.php

--- paynl12345/controllers/front/return.php
+++ paynl12345/controllers/front/return.php
@@ -14,44 +14,50 @@
 
 class paynl_paymentmethodsReturnModuleFrontController extends ModuleFrontController {
 
-    public function initContent() {
-        parent::initContent();
-        $transactionId = Tools::getValue('orderId');
+public function initContent()
+{
+parent::initContent();
+$transactionId = Tools::getValue('orderId');
 
-        try {
-            $result = Pay_Helper_Transaction::processTransaction($transactionId);
+try {
+$result = Pay_Helper_Transaction::processTransaction($transactionId);
 
-            $order = new Order($result['real_order_id']);
-            $customer = new Customer($order->id_customer);
+$order = new Order($result['real_order_id']);
+$customer = new Customer($order->id_customer);
 
-            $this->context->smarty->assign(array(
-                'reference_order' => $result['real_order_id'],
-                'email' => $customer->email,
-                'id_order_formatted'=> $order->reference,
-            ));
-            
-            if ($result['state'] == 'PAID') {
-                Tools::redirect('index.php?controller=order-confirmation&id_cart='.$result['orderId'].'&id_module='.$this->module->id.'&id_order='.$result['real_order_id'].'&key='.$customer->secure_key);
-             
-            }
-            if ($result['state'] == 'CHECKAMOUNT') {
-                $this->setTemplate('return_checkamount.tpl');
-            }
-            if ($result['state'] == 'CANCEL') {
-                if(!empty($result['real_order_id'])){
-                    Tools::redirect('index.php?controller=order&submitReorder=Reorder&id_order='.$result['real_order_id']);
-                }else {
-                    Tools::redirect('/order');
-                }
-            }
-            if ($result['state'] == 'PENDING') {
-                Tools::redirect('index.php?controller=order-confirmation&id_cart='.$result['orderId'].'&id_module='.$this->module->id.'&id_order='.$result['real_order_id'].'&key='.$customer->secure_key);
-            }
-        } catch (Exception $ex) {
+$this->context->smarty->assign(array(
+'reference_order' => $result['real_order_id'],
+'email' => $customer->email,
+'id_order_formatted'=> $order->reference,
+));
 
-            echo 'Error: ' . $ex->getMessage();
-            die();
-        }
-    }
+if ($result['state'] == 'PAID')
+{
+Tools::redirect('index.php?controller=order-confirmation&id_cart='.$result['orderId'].'&id_module='.$this->module->id.'&id_order='.$result['real_order_id'].'&key='.$customer->secure_key);
+
+}
+if ($result['state'] == 'CHECKAMOUNT')
+$this->setTemplate('return_checkamount.tpl');
+
+if ($result['state'] == 'CANCEL')
+{
+if (!empty($result['real_order_id']))
+
+Tools::redirect('index.php?controller=order&submitReorder=Reorder&id_order='.$result['real_order_id']);
+else
+
+Tools::redirect('/order');
+
+}
+if ($result['state'] == 'PENDING')
+
+Tools::redirect('index.php?controller=order-confirmation&id_cart='.$result['orderId'].'&id_module='.$this->module->id.'&id_order='.$result['real_order_id'].'&key='.$customer->secure_key);
+
+} catch (Exception $ex) {
+
+echo 'Error: '.$ex->getMessage();
+die();
+}
+}
 
 }
